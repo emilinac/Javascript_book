f66c87eb7b246c23b1a56bd415eddabb0d01e75d
Emilina Sanduleac <e.codreanu@students.clark.edu>
2023-08-07 16:07:40 -0500

    chapter 1.3 exercises part one

--- src/chapter1.js
+++ src/chapter1.js
@@ -56,35 +56,49 @@
 
 // Immutability
 
-let game = 'Super Mario';
-game.toUpperCase();
-console.log(game);
+// let game = 'Super Mario';
+// game.toUpperCase();
+// console.log(game);
 
 // copy by value
-let a = 10;
-let b = a;
-a = 100;
-console.log(b);
+// let a = 10;
+// let b = a;
+// a = 100;
+// console.log(b);
 
 // There are 7 data types string, number, biglnt(ES6), boolean, null, undefined, symbol(ES6)
 
-let num1 = new Number(1);
-let num2 = num1 + 2;
-console.log(num2);
-console.log(typeof(num1));
-console.log(typeof(num2));
-
-let universe = "universe";
-universe.toUpperCase();
-universe.length;
+// let num1 = new Number(1);
+// let num2 = num1 + 2;
+// console.log(num2);
+// console.log(typeof(num1));
+// console.log(typeof(num2));
+//
+// let universe = "universe";
+// universe.toUpperCase();
+// universe.length;
 
 // non-primitive data types
 
-let user ={
-    role:'developer'
-}
-user.employed= true;
-user.name = 'Rocko';
-console.log(user);
+// let user ={
+//     role:'developer'
+// }
+// user.employed= true;
+// user.name = 'Rocko';
+// console.log(user);
 
 // Chapter 1.3 learning
+
+// Arithmetic operators
+
+let x = 2 + 3 * 6;
+x = 2 + 18;
+x = 20;
+
+// associative operators
+
+let num1 = 100;
+let num2 = 30;
+let num3 = 80;
+let num = num1 - num2 + num3;
+
